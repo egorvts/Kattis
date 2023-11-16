10982697ce4c6853c42b29f3d0f313e00fb8c908
egorvts <egor.vetosh2310@gmail.com>
2023-11-16 16:50:54 +0500

    solved Geppetto

--- Geppetto/solution.py
+++ Geppetto/solution.py
@@ -1,28 +1,23 @@
 from itertools import combinations
 
-prohibitions = []
 n, m = map(int, input().split())
+restrictions = set()
+
 for i in range(m):
     a, b = map(int, input().split())
-    prohibitions.append((a, b))
-
-combos = set()
+    restrictions.add((a, b))
 
 def is_valid(p):
-    for i in prohibitions:
-        if (i[0] in p) and (i[1] in p):
+    for r in restrictions:
+        if (r[0] in p) and (r[1] in p):
             return False
     return True
 
-for i in range(n):
-    for p in combinations(range(1, n+1), i):
-        p = list(p)
-        if prohibitions:
-            if is_valid(p):
-                combos.add("".join(map(str, p)))
+combos = set()
 
-if combos:
-    print(len(combos))
-    print(combos)
-else:
-    print(2 ** n)
+for i in range(n+1):
+    for p in combinations(range(1, n+1), i):
+        if is_valid(p):
+            combos.add(p)
+
+print(len(combos))
